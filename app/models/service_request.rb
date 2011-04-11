e7e6688d299c6b46d93ac5934a9ec7f59b90ca4a
Herve Le Ny <hervenln@yahoo.com>
2011-04-10 21:27:53 -0700

    Fixed sr_severity errors

--- app/models/service_request.rb
+++ app/models/service_request.rb
@@ -9,28 +9,32 @@ class ServiceRequest < ActiveRecord::Base
 # TODO should look at all users who have the SR in their inbox
     # subscription = Subscription.find(:all, :conditions => ["user_id = ? and sr_severity >= ?", owner_id, severity])
     subscription = Subscription.find(:first, # TODO should handle multiple devices
-    :conditions => ["user_id=? and service_request_level>=?", owner_id, severity])
+    :conditions => ["user_id=? and sr_severity>=?", owner_id, severity])
     if subscription
-      device = APN::Device.find(:all, :conditions => ["token=?", subscription.token])
-      if device.empty?
+      devices = APN::Device.find(:all, :conditions => ["token=?", subscription.token])
+      if devices.empty?
         app = APN::App.first ## TODO should look up be name
         if app
-          device = APN::Device.create(:token => subscription.token,:app => app)
+          devices = [APN::Device.create(:token => subscription.token,:app => app)]
         end
       end
 
-      if device
-        link = {:sr_number => sr_number}
-        priority = "S#{severity}"
-        priority += "/E#{escalation.level}" unless escalation.nil?
-        notification = {:device => device, 
-          :badge=>subscription.badge,
-          :sound=>true, 
-          :alert=>"Service Request #{sr_number} (#{priority}) was just updated",
-          :custom_properties => {:sr_number => sr_number}
-        }
-        logger.info "notify_owner with  #{notification.inspect}"
-        APN::Notification.create notification
+      logger.info "devices are  #{devices.inspect}"
+
+      unless devices.empty?
+        devices.each do |device|
+          link = {:sr_number => sr_number}
+          priority = "S#{severity}"
+          priority += "/E#{escalation.level}" unless escalation.nil?
+          notification = {:device => device, 
+            :badge=>subscription.badge,
+            :sound=>true, 
+            :alert=>"Service Request #{sr_number} (#{priority}) was just updated",
+            :custom_properties => {:sr_number => sr_number}
+          }
+          logger.info "notify_owner with  #{notification.inspect}"
+          APN::Notification.create notification
+        end
       end
     end
   end
